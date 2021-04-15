8e39705eec887ab18491e14e3d93f9ed0587c536
krcasasg <krcasasg@gmail.com>
2021-04-15 00:33:23 -0500

    optimioptimize algorithm

--- app/Http/Controllers/CompareController.php
+++ app/Http/Controllers/CompareController.php
@@ -17,46 +17,74 @@ class CompareController extends Controller
 {
     public function index()
     {
-        $oldProducts = OldProduct::all('key')->toArray();
-   
-        $newProducts = NewProduct::all('key')->toArray();
+        $oldProducts = OldProduct::all()->toArray();
+
+        $newProducts = NewProduct::all()->toArray();
 
         $whithoutStock = [];
         $updateProduct = [];
+        $updateProductEquals = [];
+        $updateProductPlus = [];
+        $updateProductMinus = [];
         $newStock = [];
 
-        foreach($oldProducts as $oldprouct){
-            $newItem = array_search($oldprouct, $newProducts);
-           
-            if($newItem !== false){
-               
-                $oldItem = array_search($oldprouct, $oldProducts);
-       
-                $updateProduct[] = $oldprouct;
+        //iterate over new products list
+        //if product key is find then the product is update list
+        //asign if sum, minus or equal quantity on hand
+        foreach ($newProducts as $newProduct){
+            $encontrado = false;
+            foreach ($oldProducts as $oldProduct){
 
-                unset($oldProducts[$oldItem]);
-                unset($newProducts[$newItem]);
-            }else{
-         
-                $whithoutStock[] = $oldprouct;
-                $newItem = array_search($oldprouct['key'], $newProducts);
-                unset($newProducts[$newItem]);
+                if($newProduct['key'] == $oldProduct['key']){
+
+                    $encontrado = true;
+                    $newProduct['old_quantity_on_hand'] = $oldProduct['quantity_on_hand'];
+
+                    if($newProduct['quantity_on_hand'] == $oldProduct['quantity_on_hand']){
+                        $updateProductEquals[] = $newProduct;
+                    }elseif ($newProduct['quantity_on_hand'] > $oldProduct['quantity_on_hand']){
+                        $updateProductPlus[] = $newProduct;
+                    }else{
+                        $updateProductMinus[] = $newProduct;
+                    }
+
+                }
+            }
+
+            if(!$encontrado){
+                $newStock[] = $newProduct;
             }
         }
 
-        foreach($newProducts as $newProuct){
-            
-            $oldItem = array_search($newProuct, $oldProducts);
-            
-            if($oldItem == false){
-               $newStock[] = $newProuct;
+
+
+        foreach($oldProducts as $oldProduct) {
+
+            $encontrado = false;
+
+            foreach ($newProducts as $newProduct) {
+                if ($oldProduct['key'] == $newProduct['key']) {
+                    $encontrado = true;
+                    break;
+                }
+            }
+
+            if(!$encontrado){
+               $whithoutStock[] = $oldProduct;
             }
         }
 
-        return view('compare.results', compact('newStock', 'updateProduct', 'whithoutStock'));
-   
 
-     
+        return view('compare.results', compact(
+            'newStock',
+            'updateProductPlus',
+            'updateProductMinus',
+            'updateProductEquals',
+            'whithoutStock'
+        ));
+
+
+
 
 
     }
--- resources/views/compare/results.blade.php
+++ resources/views/compare/results.blade.php
@@ -1,74 +1,135 @@
 @extends('layouts.base')
 @section('content')
 <div class="container mx-auto">
-    <div class="border m-2 p-3">
-        <h2 class="text-center font-bold">Whitout Stock Products</h2>
+    <div class=" grid grid-1 border m-2 p-3">
+        <h2 class="text-center font-bold text-blue-600 text-xl uppercase py-2">products out of stock
+        </h2>
         @if(!empty($whithoutStock))
-        <table class="table">
-            <thead>
+            <table class="table-auto border-collapse">
+                <thead>
                 <tr>
-                    <td>Key</td>
-                   
+                    <td class="border text-center font-bold border-blue-400 p-2">Key</td>
+                    <td class="border text-center font-bold border-blue-400 p-2">Actual Quantity</td>
+
                 </tr>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 @foreach($whithoutStock as $item)
                     <tr>
-                        <td>{{ $item['key'] }}</td>
-                        
+                        <td class="border border-blue-400 p-2">{{ $item['key'] }}</td>
+                        <td class="border border-blue-400 text-right p-2">{{ $item['quantity_on_hand'] }}</td>
+
                     </tr>
                 @endforeach
-            </tbody>
-        </table>
+                </tbody>
+            </table>
         @else
-            <p>No products whitout stock</p>
+            <p class="text-center text-red-600 text-lg">There are no products.</p>
         @endif
     </div>
-    <div class="border m-2 p-3">
-        <h2 class="text-center font-bold">New Products</h2>
+
+    <div class=" grid grid-1 border m-2 p-3">
+        <h2 class="text-center font-bold text-blue-600 text-xl uppercase py-2">New products</h2>
         @if(!empty($newStock))
-        <table>
-            <thead>
+            <table class="table-auto border-collapse">
+                <thead>
                 <tr>
-                    <td>Key</td>
-                    
+                    <td class="border text-center font-bold border-blue-400 p-2">Key</td>
+                    <td class="border text-center font-bold border-blue-400 p-2">Actual Quantity</td>
+
                 </tr>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 @foreach($newStock as $item)
                     <tr>
-                        <td>{{ $item['key'] }}</td>
-                       
+                        <td class="border border-blue-400 p-2">{{ $item['key'] }}</td>
+                        <td class="border border-blue-400 text-right p-2">{{ $item['quantity_on_hand'] }}</td>
+
                     </tr>
                 @endforeach
-            </tbody>
-        </table>
+                </tbody>
+            </table>
         @else
-            <p>No New Products</p>
+            <p class="text-center text-red-600 text-lg">There are no products.</p>
         @endif
     </div>
-    <div class="border m-2 p-3">
-        <h2 class="text-center font-bold">update Products</h2>
-        @if(!empty($updateProduct))
-        <table>
-            <thead>
+
+    <div class=" grid grid-1 border m-2 p-3">
+        <h2 class="text-center font-bold text-blue-600 text-xl uppercase py-2">updated products with increased quantities</h2>
+        @if(!empty($updateProductPlus))
+            <table class="table-auto border-collapse">
+                <thead>
                 <tr>
-                    <td>Key</td>
-                    
+                    <td class="border text-center font-bold border-blue-400 p-2">Key</td>
+                    <td class="border text-center font-bold border-blue-400 p-2">Actual Quantity</td>
+                    <td class="border text-center font-bold border-blue-400 p-2">Before Quantity</td>
                 </tr>
-            </thead>
-            <tbody>
-                @foreach($updateProduct as $item)
+                </thead>
+                <tbody>
+                @foreach($updateProductPlus as $item)
                     <tr>
-                        <td>{{ $item['key'] }}</td>
-                       
+                        <td class="border border-blue-400 p-2">{{ $item['key'] }}</td>
+                        <td class="border border-blue-400 text-right p-2">{{ $item['quantity_on_hand'] }}</td>
+                        <td class="border border-blue-400 text-right p-2">{{ $item['old_quantity_on_hand'] }}</td>
                     </tr>
                 @endforeach
-            </tbody>
-        </table>
+                </tbody>
+            </table>
         @else
-            <p>No products whit updates</p>
+            <p class="text-center text-red-600 text-lg">There are no products.</p>
         @endif
     </div>
-</div>    
+
+    <div class=" grid grid-1 border m-2 p-3">
+        <h2 class="text-center font-bold text-blue-600 text-xl uppercase py-2">products updated with decreasing quantities</h2>
+        @if(!empty($updateProductMinus))
+            <table class="table-auto border-collapse">
+                <thead>
+                <tr>
+                    <td class="border text-center font-bold border-blue-400 p-2">Key</td>
+                    <td class="border text-center font-bold border-blue-400 p-2">Actual Quantity</td>
+                    <td class="border text-center font-bold border-blue-400 p-2">Before Quantity</td>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($updateProductMinus as $item)
+                    <tr>
+                        <td class="border border-blue-400 p-2">{{ $item['key'] }}</td>
+                        <td class="border border-blue-400 text-right p-2">{{ $item['quantity_on_hand'] }}</td>
+                        <td class="border border-blue-400 text-right p-2">{{ $item['old_quantity_on_hand'] }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @else
+            <p class="text-center text-red-600 text-lg">There are no products.</p>
+        @endif
+    </div>
+
+    <div class=" grid grid-1 border m-2 p-3">
+        <h2 class="text-center font-bold text-blue-600 text-xl uppercase py-2">updated products with equal quantities</h2>
+        @if(!empty($updateProductEquals))
+            <table class="table-auto border-collapse">
+                <thead>
+                <tr>
+                    <td class="border text-center font-bold border-blue-400 p-2">Key</td>
+                    <td class="border text-center font-bold border-blue-400 p-2">Actual Quantity</td>
+                    <td class="border text-center font-bold border-blue-400 p-2">Before Quantity</td>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($updateProductEquals as $item)
+                    <tr>
+                        <td class="border border-blue-400 p-2">{{ $item['key'] }}</td>
+                        <td class="border border-blue-400 text-right p-2">{{ $item['quantity_on_hand'] }}</td>
+                        <td class="border border-blue-400 text-right p-2">{{ $item['old_quantity_on_hand'] }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @else
+            <p class="text-center text-red-600 text-lg">There are no products.</p>
+        @endif
+    </div>
+</div>
 @endsection
