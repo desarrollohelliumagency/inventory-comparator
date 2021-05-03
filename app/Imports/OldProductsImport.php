7d2b3b5548a5134061ec2c0dc8c28bde19cb1492
krcasasg <krcasasg@gmail.com>
2021-05-02 22:59:48 -0500

    change chunk size 4500

--- app/Imports/OldProductsImport.php
+++ app/Imports/OldProductsImport.php
@@ -69,6 +69,6 @@ class OldProductsImport implements ToCollection, WithHeadingRow, SkipsOnError, S
 
     public function chunkSize(): int
     {
-        return 4000;
+        return 4500;
     }
 }
